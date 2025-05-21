4ec164328b91b38981b57a2492efd42781eac3eb
Uman Ruhiyat <48211686+umnrhyt@users.noreply.github.com>
2025-05-21 09:21:50 +0700

    umnrhyt

---
+++ assets/js/umnrhyt-bottom-bar.js
@@ -0,0 +1,11 @@
+/* =====================================================
+   Javascript for bottom navigation menu
+===================================================== */
+
+// Aktifkan item menu navigasi saat ini pada gulir halaman.
+
+// Javascript untuk efek menu navigasi saat menggulir.
+
+// Sembunyikan menu navigasi bawah saat mengklik tombol sembunyikan menu.
+
+// Menampilkan menu navigasi bawah saat mengklik tombol sembunyikan menu..
