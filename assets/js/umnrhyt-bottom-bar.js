4a357a8f754f662a1fa1f773feb453529c2c24ae
Uman Ruhiyat <48211686+umnrhyt@users.noreply.github.com>
2025-05-21 09:43:22 +0700

    Update umnrhyt-bottom-bar.js

--- assets/js/umnrhyt-bottom-bar.js
+++ assets/js/umnrhyt-bottom-bar.js
@@ -3,7 +3,25 @@
 ===================================================== */
 
 // Aktifkan item menu navigasi saat ini pada gulir halaman.
+window.addEventListener("scroll", () => {
+   const scrollY = window.scrollY;
+   const pageSections = document.querySelectorAll(".page-section");
+   
+   pageSections.forEach((pageSection) => {
+      let sectionTop = pageSection.offsetTop -50;
+      let sectionHeight = pageSection.offsetHeight;
+      let sectionId = pageSection.getAttribute("id");
+      
+      //console.log("Section top value :" + sectionTop);
+      //console.log("Section height value :" + sectionHeight);
 
+      if(scrollY > scrollTop){
+         document.querySelector(".bottom-menu .menu .menu-item a[href*=" + sectionId + "]").classList.add("current");
+      }
+      else{document.querySelector(".bottom-menu .menu .menu-item a[href*=" + sectionId + "]").classList.remove("current");
+      }
+  });
+});
 // Javascript untuk efek menu navigasi saat menggulir.
 
 // Sembunyikan menu navigasi bawah saat mengklik tombol sembunyikan menu.
